bd68aa422ca38b28cc74250beb4f4945bbef240f
koumey <101035209+koumey@users.noreply.github.com>
2025-04-14 03:38:24 +0700

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,6 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server đang chạy trên cổng ${PORT}`);
 });
-const express = require('express');
 const axios = require('axios');
 const app = express();
 
